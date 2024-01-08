42b65d6f841d96ca7d519e3648952aaf68f24543
nhatmobile1 <nhatmobile@gmail.com>
2024-01-08 09:29:25 -0800

    update index.html and script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+let compChoice;
+
+// function to return computer's choice
+function getComputerChoice() {
+  let choiceNum = Math.floor(Math.random() * 3) + 1;
+  if (choiceNum === 1) {
+    compChoice = "Rock";
+  } else if (choiceNum === 2) {
+    compChoice = "Paper";
+  } else {
+    compChoice = "Scissors";
+  }
+}
+
+getComputerChoice();
+console.log(compChoice);
